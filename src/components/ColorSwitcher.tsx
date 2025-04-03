d1c1556aaef5e1942e03202592de9230bbd2d924
meShantanu <shantmiitanphy@gmail.com>
2025-04-03 10:24:30 +0530

    push

--- src/components/ColorSwitcher.tsx
+++ src/components/ColorSwitcher.tsx
@@ -3,17 +3,10 @@ import { useEffect, useState } from 'react'
 import { Settings } from 'lucide-react'
 import { Button } from './ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/src/components/ui/popover'
-
-const colors = [
-  { name: 'Red', rgb: '250, 114, 117' },
-  { name: 'Blue', rgb: '59, 130, 246' },
-  { name: 'Green', rgb: '16, 185, 129' },
-  { name: 'Purple', rgb: '139, 92, 246' },
-  { name: 'Orange', rgb: '249, 115, 22' },
-]
+import { themeColors } from '../constants'
 
 export default function ColorSwitcher() {
-  const [selectedColor, setSelectedColor] = useState<string>('250, 114, 117')
+  const [selectedColor, setSelectedColor] = useState<string>('139, 92, 246')
 
   useEffect(() => {
     const storedColor = localStorage.getItem('brandColor')
@@ -38,7 +31,7 @@ export default function ColorSwitcher() {
           </Button>
         </PopoverTrigger>
         <PopoverContent className='w-40 flex flex-wrap gap-2 p-3'>
-          {colors.map((color) => (
+          {themeColors.map((color) => (
             <button
               key={color.name}
               className='w-8 h-8 rounded-full border-2 transition-all'
